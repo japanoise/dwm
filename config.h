ebf1df05e7687ffd06b06a4a732f183fde128ca6
japanoise <japanoise@tfwno.gf>
2016-10-22 11:26:48 +0100

    make spiral default

--- config.h
+++ config.h
@@ -39,10 +39,10 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[@]",      spiral },  /* first entry is default */
+	{ "[]=",      tile }, 
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "[@]",      spiral },
 };
 
 /* key definitions */
